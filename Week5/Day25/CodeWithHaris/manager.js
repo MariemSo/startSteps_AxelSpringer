73c3e51d86c77ca3e4d9f266b7a9eb8ba5d6cce5
MairemSo <souayah.mariem@gmail.com>
2024-06-11 11:04:57 +0200

    DeleteStudentName

--- Week5/Day25/CodeWithHaris/manager.js
+++ Week5/Day25/CodeWithHaris/manager.js
@@ -20,3 +20,27 @@ addButton.addEventListener("click",()=>{
     students.push(studentName);
     updateStudentList();
 })  
+
+const deleteButton = document.getElementById('delete-button');
+deleteButton.addEventListener("click",()=>{
+    students.pop();
+    updateStudentList();
+})  
+
+const deleteStudent = document.getElementById('delete-student');
+deleteStudent.addEventListener("click",()=>{
+   const studentName= document.getElementById('student-name').value.trim().toLowerCase();
+   let found = false;
+   for (var i=0;i<students.length;i++){
+       if (students[i].toLowerCase()===studentName){
+           students.splice(i,1)
+            updateStudentList();
+            found=true;
+            break;
+        }
+    }
+    if(!found){
+        const error = document.getElementById("error")
+        error.innerHTML="There is no Matching Student Name"
+    }
+})  
