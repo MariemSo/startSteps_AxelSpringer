62fa464e8c0268ad88678cc175c3522c93363105
MairemSo <souayah.mariem@gmail.com>
2024-08-08 10:30:40 +0200

    useEffect Added

--- 3-Ts_FrontEnd_OOP/Week13/D50_/nextjs_ts/components/Counter.tsx
+++ 3-Ts_FrontEnd_OOP/Week13/D50_/nextjs_ts/components/Counter.tsx
@@ -4,7 +4,20 @@ import { LangContext } from "../context/LangWrapper";
 
 const Counter: React.FC = () => {
   const [count, setCount] = useState<number>(0);
+  const [timer, setTimer] = useState<number>(0);
   const lang = useContext(LangContext);
+
+  useEffect(() => {
+    // Set up a timer
+    const timer = setInterval(() => {
+      setTimer((prevTimer) => prevTimer + 1);
+    }, 1000);
+    // Cleanup function to clear the timer
+    return () => {
+      clearInterval(timer);
+      console.log("Timer cleaned up");
+    };
+  }, []); // Empty dependency array means this effect runs only once
 
   return (
     <div>
@@ -13,6 +26,7 @@ const Counter: React.FC = () => {
       <p>
         {lang === "en" ? "The count is :" : "Die Zählung ist"} {count}
       </p>
+      <p> Timer :{timer}</p>
       <div>
         <button onClick={() => setCount(0)}>Reset</button>
         <button onClick={() => setCount(count - 1)}>-</button>
