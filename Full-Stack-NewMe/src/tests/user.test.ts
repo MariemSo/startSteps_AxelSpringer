5de54aa3ba814aef5f82cdf456dc7fe691c523e2
MairemSo <souayah.mariem@gmail.com>
2024-12-02 12:12:00 +0100

    mockDataUser_Added_Register

--- Full-Stack-NewMe/src/tests/user.test.ts
+++ Full-Stack-NewMe/src/tests/user.test.ts
@@ -1,8 +1,7 @@
-import app from "../index.js";
-import request from "supertest";
-import { AppDataSource } from "../config/dataBase.js";
-import { faker } from "@faker-js/faker";
-import { userRepo } from "../modules/User/controllers.js";
+import app from '../index.js';
+import request from 'supertest';
+import { AppDataSource } from '../config/dataBase.js';
+import { faker } from '@faker-js/faker';
 
 beforeAll(async () => {
   await AppDataSource.initialize();
@@ -12,30 +11,18 @@ afterAll(async () => {
   await AppDataSource.destroy();
 });
 
-jest.mock("../modules/User/controllers.js");
+// jest.mock('../modules/User/controllers.js');
 
-describe("User Tests", () => {
+describe('User Tests', () => {
   const randomUser = {
     email: faker.internet.email(),
     password: faker.internet.password(),
     name: faker.person.fullName(),
   };
 
-  // Clear all mocks after each test to avoid side effects
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it("Should register a User", async () => {
-    (userRepo.findOneBy as jest.Mock).mockResolvedValue(null);
-
-    (userRepo.create as jest.Mock)
-      .mockImplementation((user) => user)
-      (userRepo.save as jest.Mock)
-      .mockImplementation({ ...randomUser, id: faker.number });
-
-    const res = await request(app).post("/api/users/register").send(randomUser);
+  it('Should register a User', async () => {
+    const res = await request(app).post('/api/users/register').send(randomUser);
     expect(res.status).toBe(201);
-    expect(res.body.message).toBe("User Registered Successfully");
+    expect(res.body.message).toBe('User Registered Successfully');
   });
 });
